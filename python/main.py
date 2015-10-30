73908071cacd3530dbbd2b2ffcef4755b9ce4241
Yu Ping <yupingseu@gmail.com>
2015-10-30 17:41:42 +0800

    add x-Forworded-For

--- python/main.py
+++ python/main.py
@@ -1,8 +1,10 @@
 #-*- coding:utf-8 -*-
 import sys
 from querymain import query_main
+from logger import logger
 
 def main(argv):
-    query_main(20, 1000000, "musper", "musper真好玩")
+	logger.info("start")
+	query_main(1, 1000000, "musper", "musper真好玩")
 if __name__ == "__main__":
     main(sys.argv)
--- python/querymain.py
+++ python/querymain.py
@@ -6,11 +6,12 @@ import time
 #from PySide import QtGui, QtCore
 #from PySide.QtCore import QThread
 import threading
+from logger import logger
 
 URL_BASE = "http://m.baidu.com"
 HOST = "m.baidu.com"
 null_proxy_handler = urllib2.ProxyHandler({})
-PROXY_URL="http://vxer.daili666api.com/ip/?tid=555611790381221&num=800&delay=1"
+PROXY_URL="http://vxer.daili666api.com/ip/?tid=555611790381221&num=80&delay=1"
 
 class QueryThread(threading.Thread):
 	"""
@@ -33,7 +34,7 @@ class QueryThread(threading.Thread):
 			proxy = "http://%s" % self.proxy.strip()
 		self.cookie.clear()
 		for i in range(2):
-			time.sleep(3)
+			time.sleep(5)
 			url = self.buildUrl(self.mainKeyWord, self.assitKeyWord)
 			try:
 				self.request(url, proxy)
@@ -41,7 +42,7 @@ class QueryThread(threading.Thread):
 				print e
 				break
 			self.lastUrl = url
-			time.sleep(3)
+			time.sleep(5)
 			url = self.buildUrl(self.assitKeyWord, self.mainKeyWord)
 			try:
 				self.request(url, proxy)
@@ -52,22 +53,28 @@ class QueryThread(threading.Thread):
 
 	def buildUrl(self, word1, word2):
 		random_ts = random.randint(100000, 150000);	
-		url =  "%s/s?word=%s&sa=tb&ts=%d&rq=%s" % (URL_BASE, word1, 102345, word2)
+		url =  "%s/s?word=%s&sa=tb&ts=%d&rq=%s" % (URL_BASE, word1, random_ts, word2)
 		return url
 
 	def request(self, url, proxy):
 		if proxy:
 			self.proxyHandler = urllib2.ProxyHandler({"http":proxy})
+		proxy_ip = ""
+		if self.proxy:
+			proxy_ip = self.proxy.split(":")[0]
 		opener = urllib2.build_opener(self.proxyHandler, self.cookieHandler)
 		request = urllib2.Request(url)
 		request.add_header("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8")
 		request.add_header("User-agent", "MQQBrowser/26 Mozilla/5.0 (Linux; U; Android 2.3.7; zh-cn; MB200 Build/GRJ22; CyanogenMod-7) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1")
 		request.add_header("Referer", self.lastUrl)
 		request.add_header("Host", HOST)
+		if proxy_id:
+			request.add_header("X-Real-IP", proxy_ip)
+			request.add_header("X-Forwarded-For", proxy_ip)
 		response = opener.open(request, timeout=3)
 		print "thread: %d url:%s proxy:%s" % (self.index, url, proxy) 
 		print self.cookie
-		print response.read()[:500].decode("utf-8")
+		print response.read()[:500].decode("utf-8").encode("gbk", "ignore")
 
 def get_proxys(url):
 	"""
@@ -75,32 +82,33 @@ def get_proxys(url):
 	"""
 	response = urllib2.urlopen(url, timeout=10)
 	data = response.readlines()
-	return data
+	return dataa
 
 def query_main(threadnum, times, mainKeyWord, assitKeyWord):
-    """
-    query main
-    """
-    threads = []
-    for i in range(times):
-        try:
-            total_proxys = get_proxys(PROXY_URL)
-        except Exception, e:
-          print e
-          time.sleep(60)
-          continue
-        j = 0
-        while len(total_proxys) > 0: 
-            proxy = total_proxys.pop()
-            thread = QueryThread(j, mainKeyWord, assitKeyWord, proxy)
-            thread.setDaemon(True)
-            thread.start()
-            j = j + 1
-            threads.append(thread)
-            if j == threadnum:
-                for thread in threads:
-                    thread.join()
-                threads = []
-                j = 0
-        #sleep 3 min
-        time.sleep(180)
+	"""
+	query main
+	"""
+	threads = []
+	for i in range(times):
+		logger.info("query times:%d" % i)
+		try:
+			total_proxys = get_proxys(PROXY_URL)
+		except Exception, e:
+		  print e
+		  time.sleep(60)
+		  continue
+		j = 0
+		while len(total_proxys) > 0: 
+			proxy = total_proxys.pop()
+			thread = QueryThread(j, mainKeyWord, assitKeyWord, "")
+			thread.setDaemon(True)
+			thread.start()
+			j = j + 1
+			threads.append(thread)
+			if j == threadnum:
+				for thread in threads:
+					thread.join()
+				threads = []
+				j = 0
+		#sleep 3 min
+		time.sleep(180)
